2ce3d80b735bc30d961192132113b7d87b12adc2
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-04-19 16:30:54 +0200

    fine milestone 4 inizio bonus

--- js/main.js
+++ js/main.js
@@ -6,19 +6,6 @@ function initVue() {
             'films': [],
             'tvSeries': [],
         },
-        // mounted() {
-        //     import axios from 'axios';
-        //     const films = 'https://api.themoviedb.org/3/search/movie?api_key=1d2078d15fa15a94192ff189b968ed1f&query=suburra';
-        //     const tvSeries = 'https://api.themoviedb.org/3/search/tv?api_key=1d2078d15fa15a94192ff189b968ed1f&language=it_IT&query=suburra';
-        //     const requestFilm = axios.get(films);
-        //     const requestTvSeries = axios.get(tvSeries);
-        //     axios.all([requestFilm, requestTvSeries]).then(data => {
-        //         this.films = data.data.results;
-        //     })
-        //         .catch(error => {
-        //             console.err(error)
-        //         })
-        // },
         methods: {
             clickSearchBar: function() {
                 axios
@@ -40,36 +27,25 @@ function initVue() {
                     })
                     .then(data => {
                         this.tvSeries = data.data.results;
-                        console.log(this.tvSeries);
                     })
                     .catch(error => {
                         console.err(error)
                     })
+                    this.searchBar = '';
             },
             flag: function (language) {
                 if (language == 'en') {
-                    return '<img src="img/flag-en.png" alt="bandiera-inglese">';
+                    return '<strong>Language:</strong>' + '<img src="img/flag-en.png" alt="bandiera-inglese">';
                 }else if (language == 'it'){
-                    return '<img src="img/flag-it.png" alt="bandiera-inglese">';
+                    return '<strong>Language:</strong>' + '<img src="img/flag-it.png" alt="bandiera-inglese">';
                 }else{
-                   return '<h3>' + language +'</h3>';
+                    return '<strong>Language:</strong>' + '<h3>' + language +'</h3>';
                 }
             },
             vote: function (val) {
-                const vote = Math.floor((val * 5) / 10);
-                if (vote == 1) {
-                    return '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>'
-                }else if (vote == 2) {
-                    return '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>'
-                }else if (vote == 3) {
-                    return '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>'
-                }else if (vote == 4) {
-                    return '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>'
-                }else{
-                    return '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>'
-                }
+                const vote = Math.ceil(val / 2);
+                return vote;
             }
-
         },
         // computed: {
            
